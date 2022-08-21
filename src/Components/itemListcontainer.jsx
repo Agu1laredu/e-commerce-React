1efefe5ea3ab46990ce1879331cc85812220795a
Eduardo Aguilar <Agu1laredu@hotmail.com>
2022-08-21 01:55:58 -0300

    Creando Lading

--- src/Components/itemListcontainer.jsx
+++ src/Components/itemListcontainer.jsx
@@ -10,7 +10,7 @@ function Lading() {
       <Card.ImgOverlay>
         <Card.Title className='texto'>DREW ARGENTINA,TU TIENDA FAVORITA</Card.Title>
         <Card.Text className='texto'>
-         TIENDA NOOB se posiciona en el puesto numero uno en Argentina en distribución de la marca Drew House , Con tan solo dos años en el mercado . 
+         TIENDA NOOB se posiciona en el puesto numero uno en Argentina en distribución de la marca Drew House , con tan solo dos años en el mercado . 
          Expandiendonos a más paises de latam, pronto Brasil, Chile , Peru , Bolivia y más.
         </Card.Text>
         <Card.Text className='texto'>NUMERO UNO EN EL MERCADO, NUMERO UNO EN DISTRIBUCIÓN ARGENTINA</Card.Text>
