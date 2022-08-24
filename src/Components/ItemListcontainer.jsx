3911dee5a3a4439050bf90f1145a3c87ef5324a5
Eduardo Aguilar <Agu1laredu@hotmail.com>
2022-08-24 15:02:52 -0300

    Optimizando Slider

--- src/Components/ItemListcontainer.jsx
+++ src/Components/ItemListcontainer.jsx
@@ -1,7 +1,7 @@
 import './ItemListcontainer.css'
 import Card from 'react-bootstrap/Card';
-import Fundadores from './Components/SubComponents/Funders';
-import Slider from './Components/SubComponents/Slider';
+import Fundadores from './SubComponents/Funders';
+import Slider from './SubComponents/Slider';
 
 
 function ItemListcontainer() {
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import Lading from './ItemListcontainer'
+import Lading from './Components/ItemListcontainer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
