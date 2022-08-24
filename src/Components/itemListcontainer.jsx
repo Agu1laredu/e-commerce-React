5ad9daec48752629a30b511988f818c6ecd108dd
Eduardo Aguilar <Agu1laredu@hotmail.com>
2022-08-24 14:22:49 -0300

    Optimizando Slider

--- src/Components/itemListcontainer.jsx
+++ src/Components/itemListcontainer.jsx
@@ -5,44 +5,44 @@ import Slider from './SubComponents/Slider';
 
 
 function ItemListcontainer() {
-  
+
   const funders = {
     nombre: 'Eduardo Aguilar',
     urlimagen: 'https://via.placeholder.com/64',
-    texto:'Fundador y Director de ventas '
+    texto: 'Fundador y Director de ventas '
   }
   const fundersdos = {
     nombre: 'Bellamy Steel',
     urlimagen: 'https://via.placeholder.com/64',
-    texto:'Fundador y Director de Marketing'
+    texto: 'Fundador y Director de Marketing'
   }
   const funderstres = {
     nombre: 'Bella Thomsom',
     urlimagen: 'https://via.placeholder.com/64',
-    texto:'Fundadora y Directora de Producción'
+    texto: 'Fundadora y Directora de Producción'
   }
 
   return (
-   <div>
-     <Card className="bg-dark text-white">
-      <Slider/>
-      <div className='divtoptex'>
-      <Card.ImgOverlay className='textoo'>
-        <Card.Title className='texto'>DREW ARGENTINA,TU TIENDA FAVORITA</Card.Title>
-        <Card.Text className='texto textoo'>
-         Tienda noob se posiciona en el puesto numero uno en Argentina en distribución de joyas importadas , con más de diez años en el mercado . 
-         Expandiendonos a más paises de latam, pronto Brasil, Chile , Peru , Bolivia y más.
-        </Card.Text>
-        <Card.Text className='texto'>NUMERO UNO EN EL MERCADO, NUMERO UNO EN DISTRIBUCIÓN ARGENTINA</Card.Text>
-      </Card.ImgOverlay>
+    <div>
+      <Card className="bg-dark text-white">
+        <Slider />
+        <div className='divtoptex'>
+          <Card.ImgOverlay className='textoo'>
+            <Card.Title className='texto'>DREW ARGENTINA,TU TIENDA FAVORITA</Card.Title>
+            <Card.Text className='texto textoo'>
+              Tienda noob se posiciona en el puesto numero uno en Argentina en distribución de joyas importadas , con más de diez años en el mercado .
+              Expandiendonos a más paises de latam, pronto Brasil, Chile , Peru , Bolivia y más.
+            </Card.Text>
+            <Card.Text className='texto'>NUMERO UNO EN EL MERCADO, NUMERO UNO EN DISTRIBUCIÓN ARGENTINA</Card.Text>
+          </Card.ImgOverlay>
+        </div>
+      </Card>
+      <div className='fundadores'>
+        <Fundadores funders={funders} />
+        <Fundadores funders={fundersdos} />
+        <Fundadores funders={funderstres} />
       </div>
-    </Card>
-       <div className='fundadores'>
-       <Fundadores  funders={funders}/>
-       <Fundadores  funders={fundersdos}/>
-       <Fundadores  funders={funderstres}/>
-       </div>
-   </div>
+    </div>
   );
 }
 
