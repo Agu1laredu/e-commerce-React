8d9341d6aaf36ad95086c4de77b8444293a24536
Eduardo Aguilar <Agu1laredu@hotmail.com>
2022-09-13 21:26:43 -0300

    Creando Contact Form

--- src/Components/ItemContactcontainer.jsx
+++ src/Components/ItemContactcontainer.jsx
@@ -1,11 +1,61 @@
+import React, {useState} from 'react';
+import './ItemContact.css'
 
+const Formulario = () => {
+	const [inputNombre, cambiarInputNombre] = useState('');
+	const [inputCorreo, cambiarInputCorreo] = useState('');
 
-function Contact() {
-  return (
+	// Funcion que se encargara de validar los datos y enviar el formulario
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    <h3>Contacto</h3>
+		// Comprobamos validacion del formulario ...
+		// Si todo es correcto enviamos el formulario
 
-  );
+		console.log('Formulario Enviado!');
+	}
+
+	// Funcion que se encarga de cambiar el estado del inputNombre
+	const handleInputNombre = (e) => {
+		cambiarInputNombre(e.target.value);
+	}
+	
+	// Funcion que se encarga de cambiar el estado del inputCorreo
+	const handleInputCorreo = (e) => {
+		cambiarInputCorreo(e.target.value);
+	}
+
+	return (
+		<>
+			<form action="" onSubmit={handleSubmit} className="formulario">
+				<div className="divForm">
+					<label htmlFor="nombre">Nombre</label>
+					<input
+						type="text"
+						name="nombre"
+						placeholder="Nombre"
+						id="nombre"
+						value={inputNombre}
+						onChange={handleInputNombre}
+					/>
+				</div>
+
+				<div>
+					<label htmlFor="correo">Correo</label>
+					<input
+						type="text"
+						name="correo"
+						placeholder="Correo"
+						id="correo"
+						value={inputCorreo}
+						onChange={handleInputCorreo}
+					/>
+				</div>
+
+				<button type="submit">Enviar</button>
+			</form>
+		</>
+	);
 }
-
-export default Contact;
+ 
+export default Formulario;
